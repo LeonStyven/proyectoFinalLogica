14347456488678d4859ea861b546c1dea468c0af
LeonStyven <j.stca03@gmail.com>
2021-05-05 21:55:19 -0500

    Se reemplazo la interfaz grafica por consola

--- reproductor.java
+++ reproductor.java
@@ -1,18 +1,34 @@
-//Librerias generales
-import javax.swing.JOptionPane;
+//Importacion de librerias
+import java.util.Scanner;
 
 public class reproductor{
     public static void main(String[] args){
-        String[] opciones = {"Cliente", "Director"};
 
-        int ventana = JOptionPane.showOptionDialog(null, "Como desea ingresar?", "Seleccion de tipo de usuario", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+        int opcion = 0;
 
-        if(ventana==0){
-            System.out.println("Usuario = Cliente");
-        }else if(ventana==1){
-            System.out.println("Usuario = Director");
+        Scanner entrada = new Scanner(System.in);
+
+        do{
+            System.out.println("Desea ingresar como?...");
+            System.out.println("1. Director");
+            System.out.println("2. Oyente");
+
+            opcion = (int)entrada.nextInt();
+
+            if(!(opcion == 1 || opcion == 2)){
+                System.out.println("Usted ingreso un valor invalido...");
+                opcion = 0;
+            }
+
+        }while(opcion == 0);
+
+        if(opcion==1){
+            System.out.println("Usted ingreso como director");
+        }else{
+            System.out.println("Usted ingreso como Oyente");
         }
-
+        
+        
 
     }
 }
